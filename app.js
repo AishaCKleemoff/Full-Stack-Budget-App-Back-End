62d7fed0396adf069bf8d5bde76b70dc745c57ad
AishaCKleemoff <96323559+AishaCKleemoff@users.noreply.github.com>
2022-07-28 18:10:47 -0400

    Work in progress.

--- app.js
+++ app.js
@@ -4,9 +4,23 @@ const express = require("express");
 // CONFIGURATION
 const app = express();
 
+const cors = require("cors");
+// CORS allows my api to work. CORS helps middleware on my server
+app.use(cors());
+// helps convert JSON to JavaScript
+app.use(express.json());
+
+const transactionsController = require("./controllers/transactionsController.js");
+
 // ROUTES
 app.get("/", (req, res) => {
-  res.send("Welcome to Bookmarks App");
+  res.send("Welcome to Budgeting App");
+});
+
+app.use("/transactions", transController);
+
+app.get('*', (req, res) => {
+  res.status(404).json({ error: 'Page not found' });
 });
 
 // EXPORT
---
+++ controllers/transactionsController.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const transactions = express.Router();
+
+const transactionsData = require("../models/transactions.js");
+const { validateURL } = require("../models/validations")
+
+
+// INDEX ROUTE
+transactions.get("/", (req, res) => {
+    if (transactionsData) {
+        res.json(transactionsData);
+    } else {
+        res.redirect('/*');
+    }
+   
+
+});
+// SHOW ROUTE
+transactions.get("/:arrayIndex", (req, res) => {
+    console.log(req.params);
+    const { arrayIndex } = req.params;
+    if (transactionsData[dataIndex]) {
+        res.json(transactionsData[arrayIndex])
+    } else {
+        res.status(404).send("no transaction found - sorry");
+    } 
+})
+
+// CREATE ROUTE
+transactions.post("/", validateURL, (req, res) => {
+    transactionsData.push(req.body);
+    res.json(transactionsData[transactionsData.length - 1]);
+});
+
+// DESTROY ROUTE
+transactions.delete("/:arrayIndex", (req, res)=> {
+    const { arrayIndex } = req.params;
+    if (transactionsData[arrayIndex]) {
+        const deletedtransaction = transactionsData.splice(arrayIndex, 1)
+        res.status(200).json(deletedtransaction)
+    } else {
+        res.redirect('/*');
+    }
+    
+    
+});
+
+// UPDATE ROUTE
+transactions.put('/:arrayIndex', (req, res) => {
+    const { arrayIndex } = req.params;
+    if(transactionsData[arrayIndex]) {
+        transactionsData[arrayIndex] = req.body;
+        res.status(200).json(transactionsData[arrayIndex]);
+    } else {
+        res.redirect('/*');
+    }
+})
+
+
+
+module.exports = transactions
+// ^^ let's node.js which code to export. Also other files are given permission to access the code being exported.
